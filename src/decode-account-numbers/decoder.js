56bb87727b9dee4cdcb466d85c92cad515053904
James York <jamesoleanyork@gmail.com>
2024-05-31 12:12:03 -0400

    Add tests for decoder and digit extrapolator

--- src/decode-account-numbers/decoder.js
+++ src/decode-account-numbers/decoder.js
@@ -1,1 +1,38 @@
-module.exports = () => {};
+const asciiNumbersLookup = {
+    ' _ | ||_|':0, // 0
+    '     |  |':1, // 1
+    ' _  _||_ ':2, // 2
+    ' _  _| _|':3, // 3
+    '   |_|  |':4, // 4
+    ' _ |_  _|':5, // 5
+    ' _ |_ |_|':6, // 6
+    ' _   |  |':7, // 7
+    ' _ |_||_|':8, // 8     
+    ' _ |_| _|':9, // 9
+};
+
+const extrapolator = (asciiString) => {
+    const line1 = asciiString.substring(0, 27);
+    const line2 = asciiString.substring(27, 54);
+    const line3 = asciiString.substring(54);
+    const lines = [line1, line2, line3];
+    const asciiDigit = lines.map((line) => line.substring(0,3));
+    asciiDigitString = asciiDigit.join('');
+    return decoder(asciiDigitString).toString();;
+    
+
+
+    /*
+        newStr = str.split(''); // or newStr = [...str];
+        newStr.splice(2,5);
+        newStr = newStr.join('');
+    */
+
+};
+
+const decoder = (digitString) => asciiNumbersLookup[digitString]
+
+module.exports = {
+    extrapolator,
+    decoder
+};
--- src/decode-account-numbers/decoder.test.js
+++ src/decode-account-numbers/decoder.test.js
@@ -1,7 +1,65 @@
-const decoder = require('./decoder');
+const  {decoder, extrapolator} = require('./decoder');
 
 describe('Account Decoder', () => {
-    it('should be true', () => {
-        expect(true).toBe(false)
+    describe('digit decoder', () => {
+        it('should be able to decode the number zero (0)', () => {
+            expect(decoder(' _ '+
+                           '| |'+
+                           '|_|')).toBe(0);
+        });
+        it('should be able to decode the number one (1)', () => {
+            expect(decoder('   '+
+                           '  |'+
+                           '  |')).toBe(1);
+        });
+        it('should be able to decode the number two (2)', () => {
+            expect(decoder(' _ '+
+                           ' _|'+
+                           '|_ ')).toBe(2);
+        });
+        it('should be able to decode the number three (3)', () => {
+            expect(decoder(' _ '+
+                           ' _|'+
+                           ' _|')).toBe(3);
+        });
+        it('should be able to decode the number four (4)', () => {
+            expect(decoder('   '+
+                           '|_|'+
+                           '  |')).toBe(4);
+        });
+        it('should be able to decode the number five (5)', () => {
+            expect(decoder(' _ '+
+                           '|_ '+
+                           ' _|')).toBe(5);
+        });
+        it('should be able to decode the number six (6)', () => {
+            expect(decoder(' _ '+
+                           '|_ '+
+                           '|_|')).toBe(6);
+        });
+        it('should be able to decode the number seven (7)', () => {
+            expect(decoder(' _ '+
+                           '  |'+
+                           '  |')).toBe(7);
+        });
+        it('should be able to decode the number eight (8)', () => {
+            expect(decoder(' _ '+
+                           '|_|'+
+                           '|_|')).toBe(8);
+        });
+        it('should be able to decode the number nine (9)', () => {
+            expect(decoder(' _ '+
+                           '|_|'+
+                           ' _|')).toBe(9);
+        });
+    })
+
+    describe('digit extractor', () => {
+        it('should extract a zero as the first digit when passed an ASCII string', () => {
+            expect(extrapolator(' _  _  _  _  _  _  _  _  _ | || || || || || || || || ||_||_||_||_||_||_||_||_||_|')).toBe('0')
+        });
+        it('should extract a 1 and 2 as the first and second digit when passed an ASCII string', () => {
+            expect(extrapolator('    _  _     _  _  _  _  _   | _| _||_||_ |_   ||_||_|  ||_  _|  | _||_|  ||_| _|')).toBe('12')
+        });
     });
 });
